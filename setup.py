1b316da18055d9d3c381acbfdc85a4445977797a
dhruvparmar10 <dhruvparmar3840@gmail.com>
2025-08-31 16:58:51 +0530

    ver0.1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xfin',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit', 'pandas', 'joblib', 'shap', 'lime', 'numpy', 'matplotlib'
+    ],
+    description='Privacy-Preserving Explainable AI Library for Financial Services',
+    author='Rishabh Bhangle & Dhruv Parmar',
+    license='MIT'
+)
